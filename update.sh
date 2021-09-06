0a141f5e9ec88ba2d86c889f41540cc1e4f484c3
David Valor <david.valor@geoselfie.com.au>
2021-09-06 09:11:03 -0400

    Update

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 echo Seek out pain and fall in love with suffering
-git remote set-url origin https://davidvalorwork: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@github.com/davidvalorwork/vimrc-david.git
 git pull
 cd ~/vimrc-david
 rm .vimrc
