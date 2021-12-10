f3325e63d7888324c4b5373e07b3616b79696ce6
David Valor <david.valor@geoselfie.com.au>
2021-12-10 09:20:29 -0400

    Update

--- nvim/update-nvim-files.sh
+++ nvim/update-nvim-files.sh
@@ -10,5 +10,8 @@ cp ~/.config/nvim/init.vim ./files
 cp ~/.bashrc ./files
 cp ~/.tmux.conf.local ./files
 cp ~/.tmux.conf ./files
-cp -rf ~/.tmux ./files
+cp -r ~/.oh-my-zsh ./files
+rm -rf ./files/.oh-my-zsh/.git
+cp -r ~/.tmux ./files
+rm -rf ./files/.tmux/.git
 cp -r ~/.vim ./files
