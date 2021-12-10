814d39e13fa2483c5194444078556838aed5f604
David Valor <david.valor@geoselfie.com.au>
2021-12-10 10:02:41 -0400

    Update

--- nvim/update-nvim-files.sh
+++ nvim/update-nvim-files.sh
@@ -1,17 +1,30 @@
 # DELETE OLD CONFIGS
+echo [UpdateNvim] removing files
 rm -rf files
 
 # COPY NEW CONFIG
 mkdir files
+echo [UpdateNvim] copying ~/.gitconfig ./files
 cp ~/.gitconfig ./files
+echo [UpdateNvim] copying ~/.vimrc ./files
 cp ~/.vimrc ./files
+echo [UpdateNvim] copying ~/.zshrc ./files
 cp ~/.zshrc ./files
+echo [UpdateNvim] copying ~/.config/nvim/init.vim ./files
 cp ~/.config/nvim/init.vim ./files
+echo [UpdateNvim] copying ~/.bashrc ./files
 cp ~/.bashrc ./files
+echo [UpdateNvim] copying ~/.tmux.conf.local ./files
 cp ~/.tmux.conf.local ./files
+echo [UpdateNvim] copying ~/.tmux.conf ./files
 cp ~/.tmux.conf ./files
+echo [UpdateNvim] copying ~/.oh-my-zsh ./files
 cp -r ~/.oh-my-zsh ./files
+echo [UpdateNvim] removing ./files/.oh-my-zsh/.git
 rm -rf ./files/.oh-my-zsh/.git
+echo [UpdateNvim] copying ~/.tmux ./files
 cp -r ~/.tmux ./files
+echo [UpdateNvim] removing ./files/.tmux/.git
 rm -rf ./files/.tmux/.git
+echo [UpdateNvim] copying cp -r ~/.vim ./files
 cp -r ~/.vim ./files
