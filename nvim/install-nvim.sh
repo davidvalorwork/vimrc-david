0a6ff344ddf0b6a62c6d47d3a6eab973ddd0959b
David Valor <david.valor@geoselfie.com.au>
2021-12-13 22:12:20 -0400

    Update

--- nvim/install-nvim.sh
+++ nvim/install-nvim.sh
@@ -1,8 +1,14 @@
 # INSTALLING NEOVIM
-apt install zsh
-apt install silversearcher-ag
+cd ~
+apt install python3 -y
+apt install pip -y
+apt install zsh -y
+apt install silversearcher-ag -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 apt install neovim
 pip3 install --user neovim
 mkdir -p ~/.config/nvim
@@ -12,13 +18,8 @@ rm -f ~/.config/nvim/init.vim
 rm -f ~/.vimrc
 rm -rf -f ~/.vim
 rm -f ~/.zshrc
-rm -f ~/.bashrc
 rm -f ~/.tmux.conf.local
 rm -f ~/.tmux.conf
-rm -rf ~/.tmux
-rm -rf ~/.oh-my-zsh
-rm -rf ~/zsh-syntax-highlighting
-rm -rf ~/zsh
 rm -f ~/.gitconfig
 
 # NVIM CONFIG
@@ -28,13 +29,9 @@ cp files/init.vim ~/.config/nvim/init.vim
 cp files/.vimrc ~/.vimrc
 cp files/.zshrc ~/.zshrc
 cp files/.gitconfig ~/.gitconfig
-cp files/.bashrc ~/.bashrc
 cp files/.tmux.conf.local ~/.tmux.conf.local
 cp files/.tmux.conf ~/.tmux.conf
 cp -r ./files/zsh-syntax-highlighting ~/zsh-syntax-highlighting
-cp -r ./files/zsh ~/zsh
-cp -r files/.tmux ~/.tmux
-cp -r files/.oh-my-zsh ~/.oh-my-zsh
 cp -r files/.vim ~/.vim
 
 # OPENING VIM
