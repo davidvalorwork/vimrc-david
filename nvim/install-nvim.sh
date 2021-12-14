bc4bc3bbc420e8358bfb35a153144a961e0f0b37
David Valor <david.valor@geoselfie.com.au>
2021-12-13 22:14:20 -0400

    Update

--- nvim/install-nvim.sh
+++ nvim/install-nvim.sh
@@ -1,18 +1,3 @@
-# INSTALLING NEOVIM
-cd ~
-apt install python3 -y
-apt install pip -y
-apt install zsh -y
-apt install silversearcher-ag -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-apt install neovim
-pip3 install --user neovim
-mkdir -p ~/.config/nvim
-
 # REMOVING OLD VIM FILES
 rm -f ~/.config/nvim/init.vim
 rm -f ~/.vimrc
@@ -33,6 +18,22 @@ cp files/.tmux.conf.local ~/.tmux.conf.local
 cp files/.tmux.conf ~/.tmux.conf
 cp -r ./files/zsh-syntax-highlighting ~/zsh-syntax-highlighting
 cp -r files/.vim ~/.vim
+
+
+# INSTALLING NEOVIM
+cd ~
+apt install python3 -y
+apt install pip -y
+apt install zsh -y
+apt install silversearcher-ag -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+apt install neovim
+pip3 install --user neovim
+mkdir -p ~/.config/nvim
 
 # OPENING VIM
 nvim ~/.vimrc
