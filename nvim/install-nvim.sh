8dffdbfbe065130898c1226423045cb0230024a9
David Valor <david.valor@geoselfie.com.au>
2021-12-15 17:57:29 -0400

    Update

--- nvim/install-nvim.sh
+++ nvim/install-nvim.sh
@@ -19,6 +19,11 @@ cp files/.tmux.conf.local ~/.tmux.conf.local
 cp files/.tmux.conf ~/.tmux.conf
 cp -r files/.vim ~/.vim
 
+# DRACULA TERMINAL
+sudo apt-get install dconf-cli -y
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+./install.sh
 
 # INSTALLING NEOVIM
 cd ~
@@ -31,17 +36,14 @@ apt install pip -y
 apt install zsh -y
 apt install peco -y
 apt install silversearcher-ag -y
-sudo apt-get install dconf-cli -y
-git clone https://github.com/dracula/gnome-terminal
-cd gnome-terminal
-./install.sh
-cd ..
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
 apt install neovim
 pip3 install --user neovim
 mkdir -p ~/.config/nvim
