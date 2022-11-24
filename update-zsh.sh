47ef8dae0c4e69cdd45c5e52d0575b403250df50
David Valor <david.valor@geoselfie.com.au>
2022-11-23 21:06:50 -0400

    x

--- update-zsh.sh
+++ update-zsh.sh
@@ -7,7 +7,12 @@ read option
 if [[ $option == 1 ]]
 then
 	echo Updating zshrc
+ 	rm -rf .zshrc
+ 	rm -rf ./nvim/files/.vimrc
+ 	rm -rf ./nvim/files/maps.vim
 	cp ~/.zshrc ./
+	cp ~/.vimrc ./nvim/files/
+ 	cp ~/.vim/maps.vim ./nvim/files/.vim
 	git add .
 	git commit -m "Adding new zsh"
 	git pull
@@ -19,5 +24,9 @@ then
 	cd ~/projects/vimrc-david
 	git pull
  	rm -rf ~/.zshrc
+ 	rm -rf ~/.vimrc
+ 	rm -rf ~/.vim/maps.vim
 	cp ./.zshrc ~
+	cp ./nvim/files/.vimrc ~
+ 	cp ./nvim/files//.vim/maps.vim ~
 fi
