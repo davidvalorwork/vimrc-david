e80573f4ccd16b8b02e5ef5dc9245e926ac3decf
David Valor <david.valor@geoselfie.com.au>
2023-03-14 12:34:39 -0400

    flashcards

--- flashcards/bigo-flashcards.js
+++ flashcards/bigo-flashcards.js
@@ -7,38 +7,17 @@ import speak from './speak.js';
   flashcards.addCards(
     ['What is Big O notation for ?', ['big O notation is used to *classify* functions (usually algorithms in comp sci) by how they respond (example, in their processing time or working space requirements) to changes in input size. *efficiency*, normally of its *upper-bound* (worst case).']],
     ['What part of code is Big O used to describe ?', ['It focuses on the part of an algorithm that grows in time or space as we increase the size of the *input data* that we are operating on.']],
-    [`How Does Big O Notation Make a Runtime Analysis of an Algorithm?`, [`In order to analyze and calculate an algorithm's performance, we must calculate and compare the worst-case runtime complexities of the algorithm. The order of O(1) - known as the Constant Running Time - is the fastest running time for an algorithm, with the time taken by the algorithm being equal for different input sizes. Although the Constant Running Time is the ideal runtime for an algorithm, it can be rarely achieved because the runtime depends on the size of n inputted.  
-
-    For example, runtime analysis of an algorithm for a size of n = 20:
-    
-    n=20,
-    
-    log (20) = 2.996
-    
-    20 = 20
-    
-    20 log (20) = 59.9
-    
-    20^2 = 400
-    
-    2^20 = 1084576
-    
-    20! = 2.432902 + 1818
-    
-    Runtime complexity of some common algorithmic examples:
-    Runtime Complexity for Linear Search – O(n)
-    Runtime Complexity for Binary Search – O(log n)
-    Runtime Complexity for Bubble Sort, Insertion Sort, Selection Sort, Bucket Sort - O(n^c).
-    Runtime Complexity for Exponential algorithms like Tower of Hanoi - O(c^n).
-    Runtime Complexity for Heap Sort, Merge Sort - O(n log n).`]]
-    // ['', ['']]
-    // ['', ['']]
-    // ['', ['']]
-    // ['', ['']]
-    // ['', ['']]
-    // ['', ['']]
-    // ['', ['']]
-    // ['', ['']]
+    [`How Does Big O Notation Make a Runtime Analysis of an Algorithm?`, [`In order to analyze and calculate an algorithm's performance, we must calculate and compare the worst-case runtime complexities of the algorithm. The order of O(1) - known as the Constant Running Time - is the fastest running time for an algorithm, with the time taken by the algorithm being equal for different input sizes. Although the Constant Running Time is the ideal runtime for an algorithm, it can be rarely achieved because the runtime depends on the size of n inputted.`]],
+    ['Runtime Complexity for Linear Search?', ['O(n)']],
+    ['Runtime Complexity for Binary Search', ['O(log n)']],
+    ['Runtime Complexity for Bubble Sort, Insertion Sort, Selection Sort, Bucket Sort', ['O(n^c)']],
+    ['Runtime Complexity for Exponential algorithms like Tower of Hanoi', ['O(c^n)']],
+    ['Runtime Complexity for Heap Sort, Merge Sort', ['O(n log n)']],
+    // ['', ['']],
+    // ['', ['']],
+    // ['', ['']],
+    // ['', ['']],
+    // ['', ['']],
   );
   flashcards.shuffle();
   var currentCard = flashcards.drawNext();
